7c399b565a72c7cd06e9ba3416ee5ff6ae6c74d9
yomnamohamed07 <yomnamohamed30000@gmail.com>
2026-02-27 01:41:29 +0200

    Add History entity and controller for user trip history
    Introduced History entity to track user trip history. Added HistoryController with endpoints to retrieve and delete user history. Updated UserProfile references and cleaned up using statements.

---
+++ TransGuide.Data/Entities/Identity/History.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransGuide.Data.Entities.Identity
+{
+    public class History
+    {
+      
+        public string  UserId { get; set; }
+        public List<Trip> Trips { get; set; } = new List<Trip>();
+
+
+    }
+}
--- TransGuide.Data/Entities/Identity/UserProfile.cs
+++ TransGuide.Data/Entities/Identity/UserProfile.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using TransGuide.Data.Entities.ApplicationEntities;
 
 namespace TransGuide.Data.Entities.Identity
